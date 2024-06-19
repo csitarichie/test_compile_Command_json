670e66eeb398912e5a3d645b6f62fef7f9ab211b
Richard Szabo <szabo.richard@smb.hu>
2024-06-19 17:21:27 +0200

    initial

---
+++ test_compile_Command_json/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test_compile_Command_json
+//
+//  Created by Richard Szabo on 19.06.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
